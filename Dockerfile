41e5940f2121e15fb5e1e2c0bc2b99bf49903494
joseagarrido <josea.garrido@gmail.com>
2023-04-04 12:59:17 +0200

    menu

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM odoo:16.0
-
-LABEL MAINTAINER Marlon Falcon <mfalconsoft@gmail.com>
-USER root
-
-RUN pip3 install dropbox
---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,10 @@
+
+{
+    'name': 'prueba',
+    'depends': ['base'],
+    'data': [
+        'data/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_menu.xml',]
+}
+
